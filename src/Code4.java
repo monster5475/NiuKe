6948f1ec779ef738862faaeb2d666bbaec01c362
热心网友吴溢豪 <479400810@qq.com>
2019-09-03 13:09:22 +0800

    1、首次初始化

---
+++ src/Code4.java
@@ -0,0 +1,6 @@
+/**
+ * @author: wyh
+ * @Date: 2019/9/3 13:04
+ */
+public class Code4 {
+}
---
+++ src/MyMain.java
@@ -0,0 +1,9 @@
+/**
+ * @author: wyh
+ * @Date: 2019/9/3 13:04
+ */
+public class MyMain {
+    public static void main(String[] args) {
+
+    }
+}
