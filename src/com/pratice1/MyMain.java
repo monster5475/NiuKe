9608a60fb67b0bf0099c4ffee932a6d72f21665f
热心网友吴溢豪 <479400810@qq.com>
2019-09-04 17:58:25 +0800

    1、还未完成栈的压入和弹出

--- src/com/pratice1/MyMain.java
+++ src/com/pratice1/MyMain.java
@@ -12,40 +12,38 @@ import java.util.Stack;
  *
  * @Date: 2019/9/3 13:04
  */
+import java.util.Stack;
 public class MyMain {
 
-    public int[][] reverseArray(int[][] matrix){
-        int width = matrix.length -1;
-        if(width==0){
-            return null;
-        }
-        int height = matrix[0].length;
-        int[][] result = new int[height][width];
-        for(int i=height-1;i>=0;i--){
-            for(int j=1;j<=width;j++){
-                int ii = height-1-i;
-                int jj = j-1;
-                result[ii][jj]=matrix[j][i];
+    public boolean IsPopOrder(int [] pushA,int [] popA) {
+        Stack<Integer> stack = new Stack<>();
+        int index = 0;
+        int indexPop = 0;
+        while(index!=pushA.length && indexPop!= popA.length){
+            while (!stack.isEmpty() && stack.peek()==popA[indexPop]){
+                stack.pop();
+                indexPop++;
+            }
+            if(pushA[index] != popA[indexPop]){
+                stack.push(pushA[index]);
+                index++;
+            }else if(pushA[index] == popA[indexPop]){
+                index++;
+                indexPop++;
             }
         }
-        return result;
-    }
-
-    public ArrayList<Integer> printMatrix(int [][] matrix) {
-        ArrayList<Integer> result = new ArrayList<>();
-        while(matrix!=null){
-            for(int i=0;i<matrix[0].length;i++){
-                result.add(matrix[0][i]);
-            }
-            matrix = reverseArray(matrix);
+        if(stack.isEmpty()){
+            return true;
+        }else{
+            return false;
         }
-        return result;
     }
 
     public static void main(String[] args) {
         MyMain myMain = new MyMain();
-        int[][] matrix = {{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20}};
-        ArrayList<Integer> result = myMain.printMatrix(matrix);
-       result.stream().forEach(System.out::print);
+        int[] pushA = {1,2,3,4,5};
+        int[] popA = {4,5,3,2,1};
+        boolean flag = myMain.IsPopOrder(pushA, popA);
+        System.out.println(flag);
     }
 }
