5af6b4e4adcf1ea9fc02fc5563034014a934f0be
吴溢豪 <479400810@qq.com>
2020-04-01 22:19:22 +0800

    1、刷的LeetCode

---
+++ src/com/likou/Code33.java
@@ -0,0 +1,45 @@
+package com.likou;
+
+/**
+ * @author: wyh
+ * 搜索旋转排序数组
+ * @Day: 2020/4/1
+ */
+public class Code33 {
+    public static int findNumber(int[] nums, int left, int right, int target){
+        if(right-left==1 || right==left){
+            if(target==nums[right]){
+                return right;
+            }else if(target ==nums[left]){
+                return left;
+            }else{
+                return -1;
+            }
+        }
+        int middle = (left+right)/2;
+        int res;
+        if(nums[left]<nums[middle]){
+            if(target<=nums[middle] && target>=nums[left]){
+                res = findNumber(nums, left, middle, target);
+            }else{
+                res =findNumber(nums, middle+1, right, target);
+            }
+        }else{
+            if(target>=nums[middle+1] && target<=nums[right]){
+                res =findNumber(nums, middle+1, right, target);
+            }else{
+                res = findNumber(nums, left, middle, target);
+            }
+        }
+        return res;
+    }
+
+    public static int search(int[] nums, int target) {
+        int length = nums.length;
+        int left = 0, right = length-1;
+        if(length==0){
+            return -1;
+        }
+        return findNumber(nums, left, right, target);
+    }
+}
--- src/com/likou/Solution.java
+++ src/com/likou/Solution.java
@@ -8,66 +8,10 @@ import java.util.Scanner;
  */
 public class Solution {
 
-    public static int[] nextPermutation(int[] nums) {
-        int length = nums.length;
-        int count=0;
-        for(int i=length-1;i>0;i--){
-            if(nums[i]<=nums[i-1]){
-                count+=1;
-            }
-        }
-        if(count==length-1){
-            for(int i=0;i<length/2;i++){
-                int t = nums[i];
-                nums[i] = nums[length-1-i];
-                nums[length-1-i] = t;
-            }
-            return nums;
-        }
-        if(nums[length-1]>nums[length-2]){
-            int t = nums[length-1];
-            nums[length-1] = nums[length-2];
-            nums[length-2] = t;
-        }else{
-            int index=-1,index_=-1;
-            int min = Integer.MAX_VALUE;
-            for(int i=length-1;i>0;i--){
-                if(nums[i]>nums[i-1]){
-                    index = i-1;
-                    break;
-                }
-            }
-            for(int i=length-1;i>index;i--){
-                if(nums[i]>nums[index]&&nums[i]<min){
-                    min = nums[i];
-                    index_ = i;
-                }
-            }
-            int t = nums[index];
-            nums[index] = nums[index_];
-            nums[index_] = t;
-            for(int i=index+1;i<length;i++){
-                for(int j=i+1;j<length;j++){
-                    if(nums[i]>nums[j]){
-                        int tt = nums[i];
-                        nums[i] = nums[j];
-                        nums[j] = tt;
-                    }
-                }
-            }
-        }
-        return nums;
-    }
+
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String s= "barfoothefoobarman";
-        String[] ss = {"bar","foo"};
-        int[] nums = {1,3,2};
-        int[] nums1 = nextPermutation(nums);
-        for(int i=0;i<nums1.length;i++){
-            System.out.print(nums1[i]);
-        }
         scanner.close();
     }
 }
