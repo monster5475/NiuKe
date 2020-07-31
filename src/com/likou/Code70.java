260fa1486f4bf07abac35e29c02d1738a3d1cb8f
wuyihao <wuyihao@baidu.com>
2020-07-31 20:20:26 +0800

    + 70

---
+++ src/com/likou/Code70.java
@@ -0,0 +1,26 @@
+package com.likou;
+
+/**
+ * @Author wyh
+ * @Description 爬楼梯
+ * @Date: 2020 05 22 上午9:50
+ */
+public class Code70 {
+    public int climbStairs(int n) {
+        int n1=1;
+        int n2=2;
+        if(n==1){
+            return n1;
+        }else if(n==2){
+            return n2;
+        }else {
+            int res=0;
+            for (int i =2;i<n;i++){
+                res = n1+n2;
+                n1 = n2;
+                n2 = res;
+            }
+            return res;
+        }
+    }
+}
--- src/com/likou/Solution.java
+++ src/com/likou/Solution.java
@@ -8,32 +8,24 @@ import java.util.Scanner;
  */
 public class Solution {
 
-    public static int trap(int[] height) {
-        int length = height.length;
-        int left = 0, right =length-1;
-        int leftMax = Integer.MIN_VALUE, rightMax = Integer.MIN_VALUE;
-        int sum=0;
-        while(left<right){
-            if(height[left]<height[right]){
-                leftMax = Math.max(leftMax, height[left]);
-                sum+=leftMax-height[left];
-                left+=1;
-            }else{
-                rightMax = Math.max(rightMax, height[right]);
-                sum+=rightMax-height[right];
-                right-=1;
-            }
+
+
+    public static int reverseNum(String str) {
+        int length = str.length();
+        int half = length/2;
+        int leftDis,rightDis;
+        int begin=0;
+        int end = str.length();
+        for(int i=0;i<length;i++){
+
         }
-        return sum;
+        
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] nums = {0,1,0,2,1,0,1,3,2,1,2,1};
-        int[] height = {5,4,1,2};
-        int[] height1= {9,6,8,8,5,6,3};
-        int target = 0;
-        System.out.println(trap(height1));
+        System.out.println(reverseNum("12345"));
+
         scanner.close();
     }
 }
